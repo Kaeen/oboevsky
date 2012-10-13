ba5bcb561c4975dcc3e440df818781bf73e39a6d
Pavel Daynyak <pavel.daynyak@gmail.com>
2012-10-13 21:46:16 +0400

    Material view fix

--- oboevsky/views.py
+++ oboevsky/views.py
@@ -110,7 +110,7 @@ def category(Request, Url):
     return render_to_response('public/category.tpl', vars, RequestContext(Request, processors=[common_context_proc,]))
 
 def material(Request, Url):
-    material = get_object_or_404(Material, url=Url, visible-True)
+    material = get_object_or_404(Material, url=Url, visible=True)
 
     vars = {
         'material': material,
