7d8360e947b2add3e89ef6e3ad94d93e4edea3e3
Pavel Daynyak <pavel.daynyak@gmail.com>
2012-10-14 22:50:48 +0400

    Invisible top sells fix

--- oboevsky/views.py
+++ oboevsky/views.py
@@ -63,8 +63,8 @@ def common_context_proc(Request=None):
 
 def home(Request):
     vars = {
-        'top_sells_items': Wallpaper.objects.filter(top_sells=True),
-        'new_items': Wallpaper.objects.filter(new=True),
+        'top_sells_items': Wallpaper.objects.filter(top_sells=True, visible=True),
+        'new_items': Wallpaper.objects.filter(new=True, visible=True),
     }
 
     return render_to_response('public/index.tpl', vars, RequestContext(Request, processors=[common_context_proc,]))
