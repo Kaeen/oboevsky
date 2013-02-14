b9f0b028a7ba0374026e57df6f327bec3d6e8707
Kaeen <essz@mail.ru>
2013-02-15 00:40:15 +0400

    reg

--- oboevsky/views.py
+++ oboevsky/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404
 from django.contrib.flatpages.models import *
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 ####################################
 #       Common functionality       # 
