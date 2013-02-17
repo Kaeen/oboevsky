29a2d60d9e6bbd6b0634573ad8b61687698bf1ad
Kaeen <essz@mail.ru>
2013-02-17 17:19:23 +0400

    auth

--- oboevsky/views.py
+++ oboevsky/views.py
@@ -162,7 +162,7 @@ def login(Request):
             vars['error'] = u'Неверные логин/пароль'
             return render_to_response('public/authorize.tpl', vars, RequestContext(Request, processors=[common_context_proc,]))
 
-        user = authenticate(username=user.username, password=user.password)
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
         login(Request, user)
         #TODO: Redirect to a success page.
                 
