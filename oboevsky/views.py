3a2c178833d73c290deae332465cb022149788b4
Kaeen <essz@mail.ru>
2013-11-23 19:16:46 +0400

    hf

--- oboevsky/views.py
+++ oboevsky/views.py
@@ -138,7 +138,7 @@ def home(Request):
 def wallpaper(Request, Url):
     item = get_object_or_404(Wallpaper, url=Url, visible=True)
 
-    q = Request.POST['q']
+    q = Request.POST.get('q', [])
     collection = q.split(',')
 
     i = 0
